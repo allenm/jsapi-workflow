aef224cc415a143c21b861dd30ae2195582c316a
Allen.M <menghonglun@gmail.com>
2013-03-28 11:24:57 +0800

    remove the unused code

--- alp/__init__.py
+++ alp/__init__.py
@@ -1,11 +1,6 @@
 from .core import *
 try:
     from .item import *
-    from .keychain import *
-    from .settings import *
-    from .mail import *
-    from .fuzzy import *
-    from alp.request.request import *
 except ImportError:
     pass
 try:
